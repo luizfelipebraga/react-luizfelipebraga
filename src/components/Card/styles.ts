834ed457f26b4419b6f8dbdb5937f473954ad417
Lucas Daru Miguel <lucas.prof@gmail.com>
2021-11-08 20:27:31 -0300

    style(project component)

--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -4,23 +4,26 @@ export const ContainerCard = styled.div`
     display: flex;
     justify-content:space-between;
     flex-direction: column;
-    padding: 1rem;
 
-    max-width: 250px;
-    height: 250px;
+    max-width: 500px;
+        height: 250px;
 
     border-radius: 8px;
 
-    background-color: #252525;
 `;
 
 export const Title = styled.h2`
     color: #FFF;
-    text-align: center;
 `;
 
 export const Content = styled.span`
     color: #f9f9f9;
+    background-color: #252525;
+    font-size: .9rem;
+    padding: 1rem 2rem;
+
+    max-width: 500px;
+
 `;
 
 export const Url = styled.a`
