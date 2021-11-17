bc573e0410ac47638405591a3286b258a5b7a2c7
luizfelipebraga <ruivolfb@gmail.com>
2021-11-17 15:44:03 -0300

    feat(form) getting the current value of the states

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,7 +1,35 @@
+import { useState } from 'react';
 import { MySituation } from '../Situation';
 import { Container, NameBox, Input, Button, Select, Box } from './styles';
 
 export default function Form() {
+  const [firstName, setFirstName] = useState<string>('');
+  const [lastName, setLastName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [select, setSelect] = useState<string>('message');
+
+  const options = [
+    {
+      label: 'FullStack Job',
+      value: 'fullstack',
+    },
+
+    {
+      label: 'BackEnd Job',
+      value: 'backend',
+    },
+
+    {
+      label: 'FrontEnd Job',
+      value: 'frontend',
+    },
+
+    {
+      label: 'Other',
+      value: 'other',
+    }
+  ]
+
   return (
     <Container id="formEmail">
       <Box>
@@ -9,17 +37,30 @@ export default function Form() {
 
         <form action="submit" method="POST">
           <NameBox>
-            <Input required type="text" name="first" id="first" placeholder="First Name" />
-            <Input required type="text" name="last" id="last" placeholder="Last Name"/>
+            <Input required type="text" name="first" id="first" placeholder="First Name" value={firstName} onChange={event => setFirstName(event.target.value)} />
+            <Input required type="text" name="last" id="last" placeholder="Last Name" value={lastName} onChange={event => setLastName(event.target.value)} />
           </NameBox>
-          <Input required type="email"  pattern="/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/" name="email" id="email" placeholder="Email" />
+          <Input required type="email"
+            pattern="/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/"
+            name="email"
+            id="email"
+            placeholder="Email"
+            value={email}
+            onChange={event => setEmail(event.target.value)} />
 
-          <Select  required name="message" id="message" placeholder="Select your Message">
-            <option value={0} disabled defaultValue={0}>Whats kind message?</option>
-            <option value={1}>FullStack</option>
-            <option value={2}>FrontEnd</option>
-            <option value={3}>BackEnd</option>
-            <option value={4}>Other</option>
+          <Select
+            required
+            name="message"
+            id="message"
+            placeholder="Select your Message"
+            value={select}
+            onChange={event => setSelect(event.target.value)}>
+
+            <option value="message" disabled defaultValue="message">What type of message?</option>
+            {options.map((op) => (
+                <option value={op.value}>{op.label}</option>
+              )
+            )}
           </Select>
 
           <Button type="submit" />
